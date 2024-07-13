e914aba7f2f379eaf0ce142d0c629aaacb4ec9eb
kishu-p <priyanshu450aa@gmail.com>
2024-07-13 13:59:24 +0530

    added mongoose

--- index.js
+++ index.js
@@ -22,10 +22,11 @@ app.get("/", (req, res) => {
 	return res.send("Welcome to home Page");
 });
 
-app.get("/api/users", (req,res) => {
-	res.setHeader("X-MyName", "Kishu Singh")
-	return res.json(users)
-})
+app.get("/api/users", (req, res) => {
+	res.setHeader("X-MyName", "Kishu Singh");
+	// X added to make custom header
+	return res.json(users);
+});
 
 app.get("/users", (req, res) => {
 	return res.json(users);
@@ -49,6 +50,15 @@ app
 
 app.post("/user", (req, res) => {
 	return res.json({ status: "Pending" });
+});
+
+app.post("/userName/setting", (req, res) => {
+	const body = req.body;
+	users.push({ ...body, id: users.length + 1 });
+	fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+
+		return res.status(201).json({status: "Success", id: users.length});
+	})
 });
 
 app.listen(port, () => {
