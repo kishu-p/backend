983d628af58a32215be00b5fcfc2c208a58d8bf9
kishu-p <priyanshu450aa@gmail.com>
2024-07-14 22:51:20 +0530

    removed gitignore

---
+++ MVC/models/user.js
@@ -0,0 +1,29 @@
+// Schema
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			require: true,
+		},
+		lastName: {
+			type: String,
+		},
+		email: {
+			type: String,
+			require: true,
+			unique: true,
+		},
+		jobTitle: {
+			type: String,
+		},
+		gender: {
+			type: String,
+		},
+	},
+	{ timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- index.js
+++ index.js
@@ -17,49 +17,6 @@ app.use((req, res, next) => {
 	console.log("Hello from middleware 2");
 	next();
 });
-// REST API
-app.get("/", (req, res) => {
-	return res.send("Welcome to home Page");
-});
-
-app.get("/api/users", (req, res) => {
-	res.setHeader("X-MyName", "Kishu Singh");
-	// X added to make custom header
-	return res.json(users);
-});
-
-app.get("/users", (req, res) => {
-	return res.json(users);
-});
-
-app
-	.route("/api/users/:userId")
-	.get((req, res) => {
-		const userId = Number(req.params.userId);
-		const user = users.find((user) => user.id === userId);
-		return res.json(user);
-	})
-	.patch((req, res) => {
-		// Edit User with Id
-		return res.json({ status: "Pending" });
-	})
-	.delete((req, res) => {
-		// Delete User with Id
-		return res.json({ status: "Pending" });
-	});
-
-app.post("/user", (req, res) => {
-	return res.json({ status: "Pending" });
-});
-
-app.post("/userName/setting", (req, res) => {
-	const body = req.body;
-	users.push({ ...body, id: users.length + 1 });
-	fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-
-		return res.status(201).json({status: "Success", id: users.length});
-	})
-});
 
 app.listen(port, () => {
 	console.log(`Server started on ${port}`);
